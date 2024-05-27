7384403aa708e5d278017154a8f9e14218e8a17a
Choi-Seonghwan <vnadktdl2@snu.ac.kr>
2024-05-27 17:04:41 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let shared;
+let clickCount;
 
 function preload() {
 	partyConnect(
@@ -6,6 +7,7 @@ function preload() {
 		"hello_party"
 	);
   shared = partyLoadShared("shared", { x: 100, y: 100 });
+  clickCount = partyLoadShared("clickCount");
 }
 
 function setup() {
@@ -16,11 +18,15 @@ function setup() {
 function mousePressed() {
   shared.x = mouseX;
   shared.y = mouseY;
+  clickCount++;
 }
 
 function draw() {
   background('#ffcccc');
   fill("#000066");
+
+  textAlign(CENTER, CENTER);
+  text(clickCount, width/2, height/2);
 
   ellipse(shared.x, shared.y, 100, 100);
 }
