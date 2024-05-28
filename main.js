7532ddb61b11753d725cda35765153a7862fea35
Choi-Seonghwan <vnadktdl2@snu.ac.kr>
2024-05-29 02:23:19 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 let shared;
 let clickCount;
 let rotateDeg;
-let moveXpos;
-let moveYpos;
 
 function preload() {
   partyConnect(
@@ -12,8 +10,6 @@ function preload() {
   shared = partyLoadShared("shared", { x: 100, y: 100 });
   clickCount = partyLoadShared("clickCount", { value: 0 });
   rotateDeg = partyLoadShared("rotate", { value: 0 });
-  moveXpos = partyLoadShared("moveXpos", { x: [0] });
-  moveYpos = partyLoadShared("moveYpos", { y: [0] });
 }
 
 function setup() {
@@ -42,27 +38,15 @@ function draw() {
   text(clickCount.value, width / 2, height / 2);
   text(radians(rotateDeg.value), width / 2, 100);
 
-
-  moveXpos.x.push(radians(rotateDeg.value));
-
   if (keyIsPressed) {
     if (key === 'w') {
-      moveYpos.y.push(-0.5);
+      shared.x += 0.5*radians(rotateDeg.value);
+      shared.y -= 0.5;
     } else if (key === 's') {
-      moveYpos.y.push(0.5);
+      shared.x += 0.5*radians(rotateDeg.value);
+      shared.y += 0.5;
     }
-  } else {
-    moveYpos.y.push(0);
   }
-
-  shared.x += moveXpos.x[0];
-  shared.y += moveYpos.y[0];
-
-  moveXpos.x[0] = moveXpos.x[1];
-  moveYpos.y[0] = moveYpos.y[1];
-
-  moveXpos.x.pop();
-  moveYpos.y.pop();
 
   ellipse(shared.x, shared.y, 100, 100);
 }
