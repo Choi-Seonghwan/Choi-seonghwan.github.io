3d85dbfb74ce50362e8e90bcb604b24eca134eba
f1awguri <flawguri@flawguri.com>
2024-05-31 17:16:54 +0900

    맵 이미지, 영역 수정
    영역 투명하게 바꿨습니다

--- test map/gameMap.js
+++ test map/gameMap.js
@@ -4,17 +4,31 @@ class GameMap {
     this.height = height; //1200
     this.img = img;
     this.obstacles = [ //타일 한 칸당 80px로 계산
-      { x: 480, y: 640, width: 80, height: 160 }, //spawn left wall
-      { x: 1040, y: 640, width: 80, height: 160 } //spawn right wall
+      { x: 0, y: 0, width: 1600, height: 240 }, //top wall
+      { x: 480, y: 720, width: 80, height: 160 }, //spawn wall left
+      { x: 640, y: 400, width: 320, height: 160 }, //spawn wall middle
+      { x: 960, y: 720, width: 160, height: 160 }, //spawn wall right
+
+
+      { x: 0, y: 400, width: 320, height: 160 }, //zone 1 wall 1
+      { x: 240, y: 480, width: 80, height: 160 }, //zone 1 wall 2
+
+      { x: 0, y: 800, width: 240, height: 160 }, //zone 2 wall top
+      { x: 0, y: 1040, width: 480, height: 160 }, //zone 2 wall bottom
+
+      { x: 1280, y: 320, width: 320, height: 160 }, //zone 3 wall top
+
+      { x: 1360, y: 800, width: 240, height: 160 }, //zone 4 wall top
+      { x: 1120, y: 1040, width: 80, height: 160 }, //zone 4 wall bottom
     ];
     this.triggers = [
-      { x: 560, y: 320, width: 480, height: 480, message: "spawn zone \n press Q to interact" },
-      { x: 0, y: 0, width: 400, height: 240, message: "trigger zone 1" },
-      { x: 0, y: 480, width: 240, height: 240, message: "zone 2" },
-      { x: 0, y: 960, width: 400, height: 240, message: "zone 3" },
-      { x: 1200, y: 0, width: 400, height: 240, message: "zone 4" },
-      { x: 1360, y: 480, width: 240, height: 240, message: "zone 5" },
-      { x: 1200, y: 960, width: 400, height: 240, message: "zone 6" }
+      { x: 560, y: 480, width: 480, height: 400, message: "spawn zone \n press Q to interact" },
+
+      { x: 0, y: 560, width: 320, height: 240, message: "zone 1" }, //왼쪽 위
+      { x: 0, y: 960, width: 400, height: 240, message: "zone 2" }, //왼쪽 아래
+
+      { x: 1280, y: 480, width: 320, height: 320, message: "zone 3" }, //오른쪽 위
+      { x: 1200, y: 960, width: 400, height: 240, message: "zone 4" } //오른쪽 아래
     ];
   }
 
@@ -25,7 +39,8 @@ class GameMap {
     image(this.img, 0, 0, this.width, this.height);
 
     // obstacle
-    fill(255, 0, 0, 100);
+    // fill(255, 0, 0, 100);
+    fill(255, 0, 0, 0); //obstacle 투명화
     for (let obstacle of this.obstacles) {
       rect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
     }
@@ -34,7 +49,8 @@ class GameMap {
 
   displayTriggers() {
     // 트리거 영역 그리기
-    fill(0, 255, 0, 100);
+    // fill(0, 255, 0, 100);
+    fill(0, 255, 0, 0); //트리거 영역 투명화
     for (let trigger of this.triggers) {
       rect(trigger.x, trigger.y, trigger.width, trigger.height);
     }
