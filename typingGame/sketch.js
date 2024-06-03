3ca433db869cf31c61760637653341ef14fe3c7d
[gosaengchung] <hj65903@gmail.com>
2024-06-04 01:51:15 +0900

    button

--- typingGame/sketch.js
+++ typingGame/sketch.js
@@ -1,7 +1,18 @@
 let dungGeunMoFont;
+let buttonStart, buttonStartOver, buttonStartPressed;
+let chatBot;
 
 function preload() {
   dungGeunMoFont = loadFont('fonts/DungGeunMo.otf');
+  buttonStart = loadImage('buttons/buttonStart.png');
+  buttonStartOver = loadImage('buttons/buttonStartOver.png');
+  buttonStartPressed = loadImage('buttons/buttonStartPressed.png');
+  buttonAgain = loadImage('buttons/buttonAgain.png');
+  buttonAgainOver = loadImage('buttons/buttonAgainOver.png');
+  buttonAgainPressed = loadImage('buttons/buttonAgainPressed.png');
+  buttonClose = loadImage('buttons/buttonClose.png');
+  buttonCloseOver = loadImage('buttons/buttonCloseOver.png');
+  buttonClosePressed = loadImage('buttons/buttonClosePressed.png');
 }
 
 class ChatBot {
@@ -20,16 +31,10 @@ class ChatBot {
     this.isGameStarted = false; // 게임 시작 상태
 
     this.inputBox = createInput(); // 사용자의 입력을 받는 입력창
-    this.inputBox.position((windowWidth-(windowWidth-1000))/2, windowHeight - 150);
-    this.inputBox.size(windowWidth - 1000, 80);
+    this.inputBox.position((windowWidth - (width - 1000)) / 2, height - 150);
+    this.inputBox.size(width - 1000, 80);
     this.inputBox.changed(this.sendMessage.bind(this)); // 입력 완료 후 sendMessage() 함수 호출
     this.inputBox.hide(); // 처음에는 입력창을 숨김
-
-    this.startButton = createButton("게임 시작");
-    this.startButton.position(windowWidth / 2 - 100, windowHeight / 2 + 400);
-    this.startButton.mousePressed(() => {
-      this.startGame();
-    });
 
     this.assistantMessages.push("구매문의 주신 분 맞으세요?");
 
@@ -57,11 +62,10 @@ class ChatBot {
   }
 
   startGame() {
-    this.startButton.hide();
-    this.inputBox.show();
     this.isGameStarted = true;
     this.isGameOver = false;
     this.isGameSuccess = false;
+    this.inputBox.show();
     this.resetTimer();
   }
 
@@ -85,7 +89,7 @@ class ChatBot {
     textAlign(CENTER);
     textSize(40);
     if (this.currentInputIndex < this.expectedInputs.length) {
-      text("입력할 문구: " + this.expectedInputs[this.currentInputIndex], windowWidth / 2, windowHeight - 180);
+      text("입력할 문구: " + this.expectedInputs[this.currentInputIndex], width / 2, height - 180);
     } else {
       this.gameSuccess(); // 모든 입력이 완료되었을 때 게임 성공 처리
     }
@@ -94,27 +98,33 @@ class ChatBot {
     for (let i = 0; i < this.userMessages.length; i++) {
       fill(0);
       textAlign(RIGHT);
-      textSize(30);
-      rect(windowWidth - 650, 310 + 480 * i, 150, 150); // 내 프로필 사진
-      text(this.userMessages[i], windowWidth - 500, 500 + 500 * i);
+      textSize(40);
+      rect(width - 650, 340 + 480 * i, 150, 150); // 내 프로필 사진
+      text(this.userMessages[i], width - 500, 540 + 500 * i);
     }
 
     // 챗봇의 메시지 그리기
     for (let i = 0; i < this.assistantMessages.length; i++) {
       fill(0, 100, 255);
       textAlign(LEFT);
-      textSize(30);
-      rect(500, 70 + 480 * i, 150, 150); // 상대방 프로필 사진
-      text(this.assistantMessages[i], 500, 260 + 480 * i);
+      textSize(40);
+      rect(500, 100 + 480 * i, 150, 150); // 상대방 프로필 사진
+      text(this.assistantMessages[i], 500, 290 + 500 * i);
     }
 
-    // 타이머 막대 그래프 표시
+       // 타이머 막대 그래프 표시
     let timePassed = millis() - this.timerStart;
     let timeLeft = this.timeLimit - timePassed;
-    let barWidth = map(timeLeft, 0, this.timeLimit, 0, windowWidth - 1000);
+    let barWidth = map(timeLeft, 0, this.timeLimit, 0, width - 1000);
 
-    fill(255, 0, 0);
-    rect(500, windowHeight - 270, barWidth, 40);
+    noFill();
+    stroke(0);
+    strokeWeight(5);
+    rect(500, height - 270, width - 1000, 40); // 레트로 테두리
+
+    noStroke();
+    fill(0, 200, 0);
+    rect(500, height - 270, barWidth, 40); // 레트로 스타일 타이머 막대
 
     if (timeLeft <= 0) {
       this.gameOver();
@@ -127,11 +137,35 @@ class ChatBot {
     textSize(40);
     textAlign(CENTER);
     if (this.isGameSuccess) {
-      text("게임 성공!", windowWidth / 2, windowHeight / 2 - 50);
+      text("게임 성공!", width / 2, height / 2 - 50);
     } else {
-      text("시간 초과! 게임 오버", windowWidth / 2, windowHeight / 2 - 50);
-      this.startButton.html("다시 시작");
-      this.startButton.show();
+      text("시간 초과! 게임 오버", width / 2, height / 2 - 50); 
+    }
+
+    if (!this.isGameSuccess) {
+      // 다시 시작 버튼 표시
+      let img;
+      if (this.isButtonPressedAgain) {
+        img = buttonAgainPressed;
+      } else if (this.isButtonOverAgain) {
+        img = buttonAgainOver;
+      } else {
+        img = buttonAgain;
+      }
+
+      image(img, windowWidth / 2 - 160, windowHeight / 2 + 100, 320, 140); // 이미지 크기를 320x140px로 설정
+    } else {
+      // 게임 성공 시 닫기 버튼 표시
+      let img;
+      if (this.isButtonPressedClose) {
+        img = buttonClosePressed;
+      } else if (this.isButtonOverClose) {
+        img = buttonCloseOver;
+      } else {
+        img = buttonClose;
+      }
+      noSmooth();
+      image(img, windowWidth / 2 - 160, windowHeight / 2 + 100, 320, 140); // 이미지 크기를 320x140px로 설정
     }
   }
 
@@ -140,9 +174,20 @@ class ChatBot {
     fill(0);
     textSize(60);
     textAlign(CENTER);
-    text("챗봇 게임에 오신 것을 환영합니다!",windowWidth / 2, windowHeight / 2 - 50);
+    text("챗봇 게임에 오신 것을 환영합니다!", width / 2, height / 2 - 50);
     textSize(40);
-    text("게임 시작 버튼을 눌러주세요", windowWidth / 2, windowHeight / 2 + 50);
+    text("게임 시작 버튼을 눌러주세요", width / 2, height / 2 + 50);
+
+    let img;
+    if (this.isButtonPressed) {
+      img = buttonStartPressed;
+    } else if (this.isButtonOver) {
+      img = buttonStartOver;
+    } else {
+      img = buttonStart;
+    }
+    noSmooth();
+    image(img, windowWidth / 2 - 160, windowHeight / 2 + 100, 320, 140); // 이미지 크기를 320x140px로 설정
   }
 
   sendMessage() {
@@ -156,7 +201,7 @@ class ChatBot {
       this.currentInputIndex++; // 다음 입력해야 할 텍스트로 이동    
       this.resetTimer(); // 타이머 리셋
     } else {
-      text(this.expectedInputs[this.currentInputIndex], windowWidth / 2, windowHeight - 50);
+      text(this.expectedInputs[this.currentInputIndex], width / 2, height - 50);
     }
 
     // 채팅 수가 도합 5개를 넘으면 가장 오래된 채팅부터 사라짐
@@ -181,13 +226,10 @@ class ChatBot {
     this.gameOver();
   }
 
-  resetGame() {
-    this.initGame(); // 게임 초기화
-    this.startGame(); // 게임 시작
+  closeGame() {
+    noLoop(); // 게임 루프를 멈춥니다.
   }
 }
-
-let chatBot;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -198,3 +240,47 @@ function setup() {
 function draw() {
   chatBot.draw();
 }
+
+function mousePressed() {
+  if (!chatBot.isGameStarted && mouseX > windowWidth / 2 - 160 && mouseX < windowWidth / 2 + 160 && mouseY > windowHeight / 2 + 100 && mouseY < windowHeight / 2 + 240) {
+    chatBot.isButtonPressed = true;
+    chatBot.startGame();
+  }
+
+  if (chatBot.isGameOver && !chatBot.isGameSuccess && mouseX > windowWidth / 2 - 160 && mouseX < windowWidth / 2 + 160 && mouseY > windowHeight / 2 + 100 && mouseY < windowHeight / 2 + 240) {
+    chatBot.isButtonPressedAgain = true;
+    chatBot.initGame();
+  }
+
+  if (chatBot.isGameOver && chatBot.isGameSuccess && mouseX > windowWidth / 2 - 160 && mouseX < windowWidth / 2 + 160 && mouseY > windowHeight / 2 + 100 && mouseY < windowHeight / 2 + 240) {
+    chatBot.isButtonPressedClose = true;
+    chatBot.closeGame();
+  }
+}
+
+function mouseReleased() {
+  chatBot.isButtonPressed = false;
+  chatBot.isButtonPressedAgain = false;
+  chatBot.isButtonPressedClose = false;
+}
+
+function mouseMoved() {
+  if (!chatBot.isGameStarted && mouseX > windowWidth / 2 - 160 && mouseX < windowWidth / 2 + 160 && mouseY > windowHeight / 2 + 100 && mouseY < windowHeight / 2 + 240) {
+    chatBot.isButtonOver = true;
+  } else {
+    chatBot.isButtonOver = false;
+  }
+
+  if (chatBot.isGameOver && !chatBot.isGameSuccess && mouseX > windowWidth / 2 - 160 && mouseX < windowWidth / 2 + 160 && mouseY > windowHeight / 2 + 100 && mouseY < windowHeight / 2 + 240) {
+    chatBot.isButtonOverAgain = true;
+  } else {
+    chatBot.isButtonOverAgain = false;
+  }
+
+  if (chatBot.isGameOver && chatBot.isGameSuccess && mouseX > windowWidth / 2 - 160 && mouseX < windowWidth / 2 + 160 && mouseY > windowHeight / 2 + 100 && mouseY < windowHeight / 2 + 240) {
+    chatBot.isButtonOverClose = true;
+  } else {
+    chatBot.isButtonOverClose = false;
+  }
+}
+
